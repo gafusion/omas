f39885b86eb00d76c2fcc6b788644f44d9e6f954
Orso Meneghini <meneghini@fusion.gat.com>
2017-12-27 07:47:11 +0100

    build_json_structures uses XML-based generation routines

--- samples/build_json_structures.py
+++ samples/build_json_structures.py
@@ -1,10 +1,18 @@
 from __future__ import print_function, division, unicode_literals
 import os, re, glob
+
+os.environ['OMAS_DEBUG_TOPIC'] = '*'
 from omas import *
 
-for imas_version in map(lambda x: os.path.split(x)[-1], glob.glob(imas_json_dir + os.sep + '*')):
-    aggregate_imas_html_docs(imas_version=imas_version)
+force_build = True
 
-    create_json_structure(imas_version=imas_version)
+generate_xml_schemas()
 
-    create_html_documentation(imas_version=imas_version)
+for imas_version in sorted(map(lambda x: os.path.split(x)[-1], glob.glob(imas_json_dir + os.sep + '*'))):
+
+    print('Processing IMAS data structures v%s' % re.sub('_', '.', imas_version))
+    filename = os.path.abspath(os.sep.join([imas_json_dir, imas_version, 'omas_doc.html']))
+
+    if force_build or not os.path.exists(filename):
+        create_json_structure(imas_version=imas_version)
+        create_html_documentation(imas_version=imas_version)
