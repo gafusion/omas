a827f9d7cae15cc4b4e314ce5fe074d33f5d6fc0
Orso Meneghini <meneghini@fusion.gat.com>
2018-03-22 16:55:22 +0100

    Cleanup

--- omas/__init__.py
+++ omas/__init__.py
@@ -1,7 +1,1 @@
 from .omas_core import *
-
-import os as _os
-print(_os.environ.get('OMAS_DEBUG_TOPIC', ''))
-if _os.environ.get('OMAS_DEBUG_TOPIC', '').endswith('_dump'):
-    if _os.path.exists('omas_dump.txt'):
-        _os.remove('omas_dump.txt')
