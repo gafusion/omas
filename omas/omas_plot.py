de45b7da91296747a216d3f1a5958bb3f272b67c
Orso Meneghini <meneghini@fusion.gat.com>
2018-05-03 17:50:55 -0700

    pep8 to omas_plot

--- omas/omas_plot.py
+++ omas/omas_plot.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot
 import inspect
 from .omas_utils import *
 
-__all__=[]
+__all__ = []
 
 def add_to__all__(f):
     __all__.append(f.__name__)
@@ -42,34 +42,36 @@ def contourPaths(x, y, Z, levels, remove_boundary_points=False, smooth_factor=1)
         y = scipy.ndimage.zoom(y, sf)
         Z = scipy.ndimage.zoom(Z, sf)
 
-    [X,Y]=numpy.meshgrid(x,y)
-    Cntr = matplotlib._cntr.Cntr(X,Y,Z)
+    [X, Y] = numpy.meshgrid(x, y)
+    Cntr = matplotlib._cntr.Cntr(X, Y, Z)
 
     allsegs = []
     for level in levels:
         nlist = Cntr.trace(level)
-        nseg = len(nlist)//2
+        nseg = len(nlist) // 2
         segs = nlist[:nseg]
         if not remove_boundary_points:
             segs_ = segs
         else:
             segs_ = []
             for segarray in segs:
-                x_ = segarray[:,0]
-                y_ = segarray[:,1]
+                x_ = segarray[:, 0]
+                y_ = segarray[:, 1]
                 valid = []
-                for i in range(len(x_)-1):
-                    if numpy.isclose(x_[i],x_[i+1]) and (numpy.isclose(x_[i],max(x)) or numpy.isclose(x_[i],min(x))):
+                for i in range(len(x_) - 1):
+                    if numpy.isclose(x_[i], x_[i + 1]) and (
+                            numpy.isclose(x_[i], max(x)) or numpy.isclose(x_[i], min(x))):
                         continue
-                    if numpy.isclose(y_[i],y_[i+1]) and (numpy.isclose(y_[i],max(y)) or numpy.isclose(y_[i],min(y))):
+                    if numpy.isclose(y_[i], y_[i + 1]) and (
+                            numpy.isclose(y_[i], max(y)) or numpy.isclose(y_[i], min(y))):
                         continue
-                    valid.append((x_[i],y_[i]))
-                    if i==len(x_):
-                        valid.append(x_[i+1],y_[i+1])
+                    valid.append((x_[i], y_[i]))
+                    if i == len(x_):
+                        valid.append(x_[i + 1], y_[i + 1])
                 if len(valid):
                     segs_.append(numpy.array(valid))
 
-        segs=map(matplotlib.path.Path,segs_)
+        segs = map(matplotlib.path.Path, segs_)
         allsegs.append(segs)
     return allsegs
 
@@ -117,11 +119,8 @@ for _name, _method in inspect.getmembers(matplotlib.lines.Line2D, predicate=insp
     if _name.startswith('_'):
         continue
     setattr(Uband, _name, _method_factory(Uband, _name,
-                                         bands=_name in ['set_color',
-                                                        'set_lw',
-                                                        'set_linewidth',
-                                                        'set_dashes',
-                                                        'set_linestyle']))
+                                          bands=_name in ['set_color', 'set_lw', 'set_linewidth', 'set_dashes',
+                                                          'set_linestyle']))
 
 def uband(x, y, ax=None, fill_kw={'alpha': 0.25}, **kw):
     '''
@@ -202,61 +201,60 @@ def equilibrium_CX(ods, time_index=0, ax=None, **kw):
     :return: axes
     '''
     if ax is None:
-        ax=pyplot.gca()
+        ax = pyplot.gca()
 
-    wall=None
-    eq=ods['equilibrium']['time_slice'][time_index]
+    wall = None
+    eq = ods['equilibrium']['time_slice'][time_index]
     if 'wall' in ods:
         if time_index in ods['wall']['description_2d']:
-            wall=ods['wall']['description_2d'][time_index]['limiter']['unit']
+            wall = ods['wall']['description_2d'][time_index]['limiter']['unit']
         elif 0 in ods['wall']['description_2d']:
-            wall=ods['wall']['description_2d'][0]['limiter']['unit']
+            wall = ods['wall']['description_2d'][0]['limiter']['unit']
 
     # first try to plot as function of `rho` and fallback on `psi`
     if 'phi' in eq['profiles_2d'][0] and 'phi' in eq['profiles_1d']:
-        value2D=numpy.sqrt(abs(eq['profiles_2d'][0]['phi']))
-        value1D=numpy.sqrt(abs(eq['profiles_1d']['phi']))
+        value2D = numpy.sqrt(abs(eq['profiles_2d'][0]['phi']))
+        value1D = numpy.sqrt(abs(eq['profiles_1d']['phi']))
     else:
-        value2D=eq['profiles_2d'][0]['psi']
-        value1D=eq['profiles_1d']['psi']
-    value2D=(value2D-min(value1D))/(max(value1D)-min(value1D))
-    levels=numpy.r_[0.1:10:0.1]
+        value2D = eq['profiles_2d'][0]['psi']
+        value1D = eq['profiles_1d']['psi']
+    value2D = (value2D - min(value1D)) / (max(value1D) - min(value1D))
+    levels = numpy.r_[0.1:10:0.1]
 
     # contours
-    line=numpy.array([numpy.nan,numpy.nan])
-    for item1 in contourPaths(eq['profiles_2d'][0]['grid']['dim1'],
-                              eq['profiles_2d'][0]['grid']['dim2'],
-                              value2D, levels, smooth_factor=1):
+    line = numpy.array([numpy.nan, numpy.nan])
+    for item1 in contourPaths(eq['profiles_2d'][0]['grid']['dim1'], eq['profiles_2d'][0]['grid']['dim2'], value2D,
+                              levels, smooth_factor=1):
         for item in item1:
-            line=numpy.vstack(( line, item.vertices,numpy.array([numpy.nan,numpy.nan]) ))
+            line = numpy.vstack((line, item.vertices, numpy.array([numpy.nan, numpy.nan])))
 
     # internal flux surfaces w/ or w/o masking
     if wall is not None:
-        path  = matplotlib.path.Path(numpy.transpose(numpy.array([wall[0]['outline']['r'],wall[0]['outline']['z']])))
+        path = matplotlib.path.Path(numpy.transpose(numpy.array([wall[0]['outline']['r'], wall[0]['outline']['z']])))
         patch = matplotlib.patches.PathPatch(path, facecolor='none')
         ax.add_patch(patch)
-        pyplot.plot(line[:,0],line[:,1],**kw)
+        pyplot.plot(line[:, 0], line[:, 1], **kw)
         ax.lines[-1].set_clip_path(patch)
     else:
-        pyplot.plot(line[:,0],line[:,1],**kw)
+        pyplot.plot(line[:, 0], line[:, 1], **kw)
 
     # plotting style
-    kw1=copy.deepcopy(kw)
-    kw1['linewidth']=kw.setdefault('linewidth',1)+1
-    kw1.setdefault('color',ax.lines[-1].get_color())
+    kw1 = copy.deepcopy(kw)
+    kw1['linewidth'] = kw.setdefault('linewidth', 1) + 1
+    kw1.setdefault('color', ax.lines[-1].get_color())
 
     # boundary
-    ax.plot(eq['boundary']['outline']['r'],eq['boundary']['outline']['z'],**kw1)
+    ax.plot(eq['boundary']['outline']['r'], eq['boundary']['outline']['z'], **kw1)
 
     # axis
-    ax.plot(eq['global_quantities']['magnetic_axis']['r'],eq['global_quantities']['magnetic_axis']['z'],'+',**kw1)
+    ax.plot(eq['global_quantities']['magnetic_axis']['r'], eq['global_quantities']['magnetic_axis']['z'], '+', **kw1)
 
     # wall
     if wall is not None:
-        ax.plot(wall[0]['outline']['r'],wall[0]['outline']['z'],'k',linewidth=2)
+        ax.plot(wall[0]['outline']['r'], wall[0]['outline']['z'], 'k', linewidth=2)
 
-        ax.axis([min(wall[0]['outline']['r']), max(wall[0]['outline']['r']),
-                        min(wall[0]['outline']['z']), max(wall[0]['outline']['z'])])
+        ax.axis([min(wall[0]['outline']['r']), max(wall[0]['outline']['r']), min(wall[0]['outline']['z']),
+                 max(wall[0]['outline']['z'])])
 
     # axes
     ax.set_aspect('equal')
@@ -283,53 +281,53 @@ def equilibrium_summary(ods, time_index=0, fig=None, **kw):
     :return: figure handler
     '''
     if fig is None:
-        fig=pyplot.figure()
+        fig = pyplot.figure()
 
-    ax=pyplot.subplot(1,3,1)
-    ax=equilibrium_CX(ods, time_index=time_index, ax=ax, **kw)
-    eq=ods['equilibrium']['time_slice'][time_index]
+    ax = pyplot.subplot(1, 3, 1)
+    ax = equilibrium_CX(ods, time_index=time_index, ax=ax, **kw)
+    eq = ods['equilibrium']['time_slice'][time_index]
 
     # x
     if 'phi' in eq['profiles_2d'][0] and 'phi' in eq['profiles_1d']:
-        x=numpy.sqrt(abs(eq['profiles_1d']['phi']))
-        xName='$\\rho$'
+        x = numpy.sqrt(abs(eq['profiles_1d']['phi']))
+        xName = '$\\rho$'
     else:
-        x=eq['profiles_1d']['psi']
-        xName='$\\psi$'
-    x=(x-min(x))/(max(x)-min(x))
+        x = eq['profiles_1d']['psi']
+        xName = '$\\psi$'
+    x = (x - min(x)) / (max(x) - min(x))
 
     # pressure
-    ax=pyplot.subplot(2,3,2)
-    ax.plot(x,eq['profiles_1d']['pressure'], **kw)
-    kw.setdefault('color',ax.lines[-1].get_color())
+    ax = pyplot.subplot(2, 3, 2)
+    ax.plot(x, eq['profiles_1d']['pressure'], **kw)
+    kw.setdefault('color', ax.lines[-1].get_color())
     ax.set_title('$\,$ Pressure')
-    ax.ticklabel_format(style='sci', scilimits=(-1,2), axis='y')
+    ax.ticklabel_format(style='sci', scilimits=(-1, 2), axis='y')
     pyplot.setp(ax.get_xticklabels(), visible=False)
 
     # q
-    ax=fig.add_subplot(2,3,3,sharex=ax)
-    ax.plot(x,eq['profiles_1d']['q'], **kw )
+    ax = fig.add_subplot(2, 3, 3, sharex=ax)
+    ax.plot(x, eq['profiles_1d']['q'], **kw)
     ax.set_title('$q$ Safety factor')
-    ax.ticklabel_format(style='sci', scilimits=(-1,2), axis='y')
+    ax.ticklabel_format(style='sci', scilimits=(-1, 2), axis='y')
     if 'label' in kw:
         ax.legend(loc=0).draggable(True)
     pyplot.setp(ax.get_xticklabels(), visible=False)
 
     # dP_dpsi
-    ax=fig.add_subplot(2,3,5,sharex=ax)
-    ax.plot(x,eq['profiles_1d']['dpressure_dpsi'], **kw )
+    ax = fig.add_subplot(2, 3, 5, sharex=ax)
+    ax.plot(x, eq['profiles_1d']['dpressure_dpsi'], **kw)
     ax.set_title("$P\,^\\prime$ source function")
-    ax.ticklabel_format(style='sci', scilimits=(-1,2), axis='y')
+    ax.ticklabel_format(style='sci', scilimits=(-1, 2), axis='y')
     pyplot.xlabel(xName)
 
     # FdF_dpsi
-    ax=fig.add_subplot(236,sharex=ax)
-    ax.plot(x,eq['profiles_1d']['f_df_dpsi'], **kw)
+    ax = fig.add_subplot(236, sharex=ax)
+    ax.plot(x, eq['profiles_1d']['f_df_dpsi'], **kw)
     ax.set_title("$FF\,^\\prime$ source function")
-    ax.ticklabel_format(style='sci', scilimits=(-1,2), axis='y')
+    ax.ticklabel_format(style='sci', scilimits=(-1, 2), axis='y')
     pyplot.xlabel(xName)
 
-    ax.set_xlim([0,1])
+    ax.set_xlim([0, 1])
 
     return fig
 
@@ -352,53 +350,53 @@ def core_profiles_summary(ods, time_index=0, fig=None, combine_dens_temps=True, 
     :return: figure handler
     '''
     if fig is None:
-        fig=pyplot.figure()
+        fig = pyplot.figure()
 
-    prof1d=ods['core_profiles']['profiles_1d'][time_index]
-    x=prof1d['grid.rho_tor_norm']
+    prof1d = ods['core_profiles']['profiles_1d'][time_index]
+    x = prof1d['grid.rho_tor_norm']
 
-    what=['electrons']+['ion[%d]'%k for k in range(len(prof1d['ion']))]
-    names=['Electrons']+[prof1d['ion[%d].label'%k]+' ion' for k in range(len(prof1d['ion']))]
+    what = ['electrons'] + ['ion[%d]' % k for k in range(len(prof1d['ion']))]
+    names = ['Electrons'] + [prof1d['ion[%d].label' % k] + ' ion' for k in range(len(prof1d['ion']))]
 
-    r=len(prof1d['ion'])+1
+    r = len(prof1d['ion']) + 1
 
-    ax=None
-    for k,item in enumerate(what):
+    ax = None
+    for k, item in enumerate(what):
 
-        #densities (thermal and fast)
-        for therm_fast in ['','_fast']:
-            therm_fast_name=['',' (fast)'][therm_fast=='_fast']
-            density=item+'.density'+therm_fast
-            if item+'.density'+therm_fast in prof1d:
+        # densities (thermal and fast)
+        for therm_fast in ['', '_fast']:
+            therm_fast_name = ['', ' (fast)'][therm_fast == '_fast']
+            density = item + '.density' + therm_fast
+            if item + '.density' + therm_fast in prof1d:
                 if combine_dens_temps:
-                    if k==0:
-                        ax=ax0=pyplot.subplot(1,2,1)
+                    if k == 0:
+                        ax = ax0 = pyplot.subplot(1, 2, 1)
                 else:
-                    ax=ax0=pyplot.subplot(r,2,(2*k)+1,sharex=ax)
-                uband(x,prof1d[density],label=names[k]+therm_fast_name,ax=ax0,**kw)
-                if k==len(prof1d['ion']):
+                    ax = ax0 = pyplot.subplot(r, 2, (2 * k) + 1, sharex=ax)
+                uband(x, prof1d[density], label=names[k] + therm_fast_name, ax=ax0, **kw)
+                if k == len(prof1d['ion']):
                     ax0.set_xlabel('$\\rho$')
                     if combine_dens_temps:
                         ax0.legend(loc=0).draggable(True)
-                if k==0:
+                if k == 0:
                     ax0.set_title('Density [m$^{-3}$]')
                 if not combine_dens_temps:
                     ax0.set_ylabel(names[k])
 
-        #temperatures
-        if item+'.temperature' in prof1d:
+        # temperatures
+        if item + '.temperature' in prof1d:
             if combine_dens_temps:
-                if k==0:
-                    ax=ax1=pyplot.subplot(1,2,2,sharex=ax0)
+                if k == 0:
+                    ax = ax1 = pyplot.subplot(1, 2, 2, sharex=ax0)
             else:
-                ax=ax1=pyplot.subplot(r,2,(2*k)+2,sharex=ax)
-            uband(x,prof1d[item+'.temperature'],label=names[k],ax=ax1,**kw)
-            if k==len(prof1d['ion']):
+                ax = ax1 = pyplot.subplot(r, 2, (2 * k) + 2, sharex=ax)
+            uband(x, prof1d[item + '.temperature'], label=names[k], ax=ax1, **kw)
+            if k == len(prof1d['ion']):
                 ax1.set_xlabel('$\\rho$')
-            if k==0:
+            if k == 0:
                 ax1.set_title('Temperature [eV]')
 
-    ax.set_xlim([0,1])
+    ax.set_xlim([0, 1])
     return fig
 
 @add_to__all__
