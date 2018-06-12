6f1509cfc13346bfe477f4f7487b9e97ee73e5ac
Orso Meneghini <meneghini@fusion.gat.com>
2018-06-12 15:58:39 -0700

    Added tests for different save/load formats

--- tests/test_omas_suite.py
+++ tests/test_omas_suite.py
@@ -6,13 +6,51 @@ import unittest
 
 from omas import *
 
+try:
+    import imas
+    HAVE_IMAS = True
+except ImportError:
+    HAVE_IMAS = False
+
+try:
+    import hdc
+    HAVE_HDC = True
+except ImportError:
+    HAVE_HDC = False
+
+try:
+    import ual
+    HAVE_ITM = True
+except ImportError:
+    HAVE_ITM = False
+
+ods = ods_sample()
 
 class TestOmasSuite(unittest.TestCase):
 
-    def test_omas(self):
-        through_omas_suite(ods=None, test_type=None, do_raise=False)
-        # do_raise = False will prevent exceptions! The test does nothing but look pretty.
+    def test_omas_pkl(self):
+        through_omas_pkl(ods)
 
+    def test_omas_json(self):
+        through_omas_json(ods)
+
+    def test_omas_nc(self):
+        through_omas_nc(ods)
+
+    def test_omas_s3(self):
+        through_omas_s3(ods)
+
+    @unittest.skipUnless(HAVE_IMAS, "requires imas")
+    def test_omas_imas(self):
+        through_omas_imas(ods)
+
+    @unittest.skipUnless(HAVE_HDC, "requires hdc")
+    def test_omas_hdc(self):
+        through_omas_hdc(ods)
+
+    @unittest.skipUnless(HAVE_ITM, "requires ual")
+    def test_omas_itm(self):
+        through_omas_itm(ods)
 
 if __name__ == '__main__':
     unittest.main()
