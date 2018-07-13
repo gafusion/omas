ec7a1c7b0699a775f04fcb0f5585a658539ac440
Sterling Smith <smithsp@fusion.gat.com>
2018-07-13 12:26:39 -0700

    Fix travis builds to not error out at once (#43)
    * Fixed imports in omas/tests/__init__.py

--- omas/tests/__init__.py
+++ omas/tests/__init__.py
@@ -1,5 +1,13 @@
-from .test_omas_core import *
-from .test_omas_physics import *
-from .test_omas_plot import *
-from .test_omas_suite import *
-from .test_omas_utils import *
+import sys
+if sys.version_info.major==2:
+    from test_omas_core import *
+    from test_omas_physics import *
+    from test_omas_plot import *
+    from test_omas_suite import *
+    from test_omas_utils import *
+else:
+    from .test_omas_core import *
+    from .test_omas_physics import *
+    from .test_omas_plot import *
+    from .test_omas_suite import *
+    from .test_omas_utils import *
