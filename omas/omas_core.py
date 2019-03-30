c0d896ac95a4937073f25b9b5fbf645808392824
Orso Meneghini <orso82@gmail.com>
2019-03-30 11:14:54 -0700

    cleanup unecessary call to p2l

--- omas/omas_core.py
+++ omas/omas_core.py
@@ -973,8 +973,6 @@ class ODS(MutableMapping):
         :return: time dependent array
         '''
 
-        key = p2l(key)
-
         orig_value = []
         if key in self:
             orig_value = numpy.atleast_1d(self[key]).tolist()
@@ -986,6 +984,7 @@ class ODS(MutableMapping):
         elif time_index == len(orig_value):
             orig_value = orig_value + [value]
         else:
+            key = p2l(key)
             raise IndexError('%s has length %d and time_index %d is bejond current range' % (l2o(key), len(orig_value), time_index))
 
         self[key] = numpy.atleast_1d(orig_value)
