cdad2cecae125662a984af30ac9be3130060db8e
Orso Meneghini <orso82@gmail.com>
2019-05-01 18:20:11 -0400

    pf_active dataset build with non-homogeneous time

--- omas/omas_sample.py
+++ omas/omas_sample.py
@@ -131,8 +131,8 @@ def core_profiles(ods, time_index=0, nx=11, add_junk_ion=False, include_pressure
 @add_to_ODS
 def pf_active(ods, nc_weird=0, nc_undefined=0):
     """
-    Adds some FAKE active PF coil locations so that the overlay plot will work in tests. It's fine to test
-    with dummy data as long as you know it's not real.
+    Adds some FAKE active PF coil locations so that the overlay plot will work in tests.
+    It's fine to test with dummy data as long as you know it's not real.
 
     :param ods: ODS instance
 
@@ -147,12 +147,12 @@ def pf_active(ods, nc_weird=0, nc_undefined=0):
     """
 
     nc_reg = 4
-    nc = nc_reg+nc_weird+nc_undefined
+    nc = nc_reg + nc_weird + nc_undefined
     fc_dat = [
         #  R        Z       dR      dZ    tilt1  tilt2
-        [.8608,  .16830,  .0508,  .32106,  0.0,  0.0],
-        [1.0041,  1.5169,  .13920,  .11940,  45.0,  0.0],
-        [2.6124,  0.4376,  0.17320,  0.1946,  0.0,  92.40],
+        [.8608, .16830, .0508, .32106, 0.0, 0.0],
+        [1.0041, 1.5169, .13920, .11940, 45.0, 0.0],
+        [2.6124, 0.4376, 0.17320, 0.1946, 0.0, 92.40],
         [2.3834, -1.1171, 0.1880, 0.16920, 0.0, -108.06],
     ]
 
@@ -170,14 +170,14 @@ def pf_active(ods, nc_weird=0, nc_undefined=0):
         else:
             outline = ods['pf_active.coil'][i]['element.0.geometry.outline']
             fdat = fc_dat[i]
-            fdat[4] = -fc_dat[i][4] * numpy.pi / 180
-            fdat[5] = -(fc_dat[i][5] * numpy.pi / 180 if fc_dat[i][5] != 0 else numpy.pi / 2)
+            fdat[4] = -fc_dat[i][4] * numpy.pi / 180.
+            fdat[5] = -(fc_dat[i][5] * numpy.pi / 180. if fc_dat[i][5] != 0 else numpy.pi / 2.)
             outline['r'] = [
-                fdat[0] - fdat[2] / 2. - fdat[3] / 2. * numpy.tan((numpy.pi/2. + fdat[5])),
-                fdat[0] - fdat[2] / 2. + fdat[3] / 2. * numpy.tan((numpy.pi/2. + fdat[5])),
-                fdat[0] + fdat[2] / 2. + fdat[3] / 2. * numpy.tan((numpy.pi/2. + fdat[5])),
-                fdat[0] + fdat[2] / 2. - fdat[3] / 2. * numpy.tan((numpy.pi/2. + fdat[5])),
-             ]
+                fdat[0] - fdat[2] / 2. - fdat[3] / 2. * numpy.tan((numpy.pi / 2. + fdat[5])),
+                fdat[0] - fdat[2] / 2. + fdat[3] / 2. * numpy.tan((numpy.pi / 2. + fdat[5])),
+                fdat[0] + fdat[2] / 2. + fdat[3] / 2. * numpy.tan((numpy.pi / 2. + fdat[5])),
+                fdat[0] + fdat[2] / 2. - fdat[3] / 2. * numpy.tan((numpy.pi / 2. + fdat[5])),
+            ]
             outline['z'] = [
                 fdat[1] - fdat[3] / 2. - fdat[2] / 2. * numpy.tan(-fdat[4]),
                 fdat[1] + fdat[3] / 2. - fdat[2] / 2. * numpy.tan(-fdat[4]),
@@ -186,14 +186,20 @@ def pf_active(ods, nc_weird=0, nc_undefined=0):
             ]
             ods['pf_active.coil'][i]['element.0.geometry.geometry_type'] = outline_code
 
-    for i in range(nc_reg, nc_reg+nc_weird):
+    for i in range(nc_reg, nc_reg + nc_weird):
         # This isn't a real geometry_type, so it should trigger the contingency
         ods['pf_active.coil'][i]['element.0.geometry.geometry_type'] = 99
-    for i in range(nc_reg+nc_weird, nc):
+    for i in range(nc_reg + nc_weird, nc):
         # This one doesn't have geometry_type defined, so the plot overlay will have trouble looking up which type it is
         outline = ods['pf_active.coil'][i]['element.0.geometry.outline']
         outline['r'] = [1.5, 1.6, 1.7, 1.5]
         outline['z'] = [0.1, 0.3, -0.1, 0]
+
+    # generate some data that is not time homogeneous
+    for i in range(nc_reg):
+        n = (1 + i) * 10
+        outline = ods['pf_active.coil'][i]['current.data'] = numpy.linspace(0, 1, n)
+        outline = ods['pf_active.coil'][i]['current.data'] = numpy.linspace(0, 1, n)
 
     return ods
 
