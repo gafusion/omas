eae10d863042ba18439257e54db80207f47fbd94
Orso Meneghini <orso82@gmail.com>
2019-05-02 20:51:35 -0400

    Speedup ODX data access

--- omas/omas_ds.py
+++ omas/omas_ds.py
@@ -16,25 +16,32 @@ class ODX(MutableMapping):
     """
     def __init__(self, DS):
         self.omas_data = DS
+        self.ucache = {}
+        for k in self.omas_data.data_vars:
+            self.ucache.setdefault(o2u(k),[]).append(k)
 
     def __delitem__(self, key):
         pass
 
     def __getitem__(self, key):
+        # return data if key is exactly in dataset
         if key in self.omas_data:
             return self.omas_data[key].values
 
+        # identify which element in DS has the requested data
         ukey = None
-        for k in self.omas_data.data_vars:
+        for k in self.ucache[o2u(key)]:
             if u2o(key, k) == k:
                 ukey = k
                 break
 
+        # slice the data as requested
         tmp = self.omas_data[ukey].values
         for uk, k in zip(p2l(ukey), p2l(key)):
             if uk == ':' and isinstance(k, int):
                 tmp = tmp[k]
 
+        # return data
         return tmp
 
     def __iter__(self):
