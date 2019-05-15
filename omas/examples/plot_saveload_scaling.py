9459ea91040e30a3aa791e51e137ae7dd412d9ef
Orso Meneghini <orso82@gmail.com>
2019-05-15 14:41:46 -0700

    Streamlined scaling plot

--- omas/examples/plot_saveload_scaling.py
+++ omas/examples/plot_saveload_scaling.py
@@ -3,8 +3,8 @@
 """
 Storage performance scaling
 ===========================
-This example shows scaling performance of different
-OMAS storage formats.
+This example shows a scaling performance study for
+storaging data in hieararchical or tensor format
 """
 
 import os
@@ -18,113 +18,117 @@ from matplotlib import pyplot
 ods = ODS()
 ods.sample_equilibrium()
 
-max_t_wait = 2
-max_n = 2
+max_n = 100
 max_samples = 11
-stats_reps = 100
-samples = numpy.unique(list(map(int, numpy.logspace(0, max_n, max_samples)))).tolist()
+stats_reps = 10
+samples = numpy.unique(list(map(int, numpy.logspace(0, numpy.log10(max_n), max_samples)))).tolist()
+max_samples = len(samples)
 
 times = {}
-times['h5_save'] = []
-times['h5_load'] = []
-times['ds_save'] = []
-times['ds_load'] = []
-times['dx_load'] = []
-times['ods_slice_access'] = []
-times['ods_bulk_access'] = []
-times['odx_slice_access'] = []
-times['odx_bulk_access'] = []
+for type in ['H', 'T']:  # hierarchical or tensor
+    for action in ['R', 'W', 'M', 'A', 'S', 'B']:  # Read, Write, Mapping, Array access, Stripe access, Bulk access
+        times[type + action] = []
 
-#print(samples)
 for n in samples:
-    #print(n)
+    print('%d/%d'%(n,samples[-1]))
 
     # keep adding time slices
     for k in range(len(ods['equilibrium.time_slice']), n):
         ods.sample_equilibrium(time_index=k)
 
     # save load in HDF5
-    if not len(times['h5_save']) or times['h5_save'][-1] < max_t_wait:
-        t0 = time.time()
-        save_omas_h5(ods, tempfile.gettempdir() + os.sep + 'tmp.h5')
-        times['h5_save'].append(time.time() - t0)
+    filename = tempfile.gettempdir() + os.sep + 'tmp.h5'
+    t0 = time.time()
+    save_omas_h5(ods, filename)
+    times['HW'].append(time.time() - t0)
 
-        t0 = time.time()
-        load_omas_h5(tempfile.gettempdir() + os.sep + 'tmp.h5')
-        times['h5_load'].append(time.time() - t0)
+    t0 = time.time()
+    load_omas_h5(filename)
+    times['HR'].append(time.time() - t0)
 
-        t0 = time.time()
-        for k in range(stats_reps):
-            for kk in range(n):
-                ods['equilibrium.time_slice.%d.profiles_1d.psi' % kk]
-        times['ods_slice_access'].append((time.time() - t0) / n / float(stats_reps))
+    t0 = time.time()
+    for k in range(stats_reps):
+        for kk in range(n):
+            ods['equilibrium.time_slice.%d.profiles_1d.psi' % kk]
+    times['HA'].append((time.time() - t0) / n / float(stats_reps))
 
-        t0 = time.time()
-        for k in range(stats_reps):
-            ods['equilibrium.time_slice.:.profiles_1d.psi']
-        times['ods_bulk_access'].append((time.time() - t0) / float(stats_reps))
+    t0 = time.time()
+    for kk in range(n):
+        ods['equilibrium.time_slice.:.profiles_1d.psi'][:, 0]
+    times['HS'].append((time.time() - t0) / n)
 
-    else:
-        times['h5_save'].append(numpy.nan)
-        times['h5_load'].append(numpy.nan)
-        times['ods_slice_access'].append(numpy.nan)
-        times['ods_bulk_access'].append(numpy.nan)
+    t0 = time.time()
+    for k in range(stats_reps):
+        ods['equilibrium.time_slice.:.profiles_1d.psi']
+    times['HB'].append((time.time() - t0) / float(stats_reps))
 
-    # save load in xarray dataset format
-    if not len(times['ds_save']) or times['ds_save'][-1] < max_t_wait:
-        t0 = time.time()
-        save_omas_ds(ods, tempfile.gettempdir() + os.sep + 'tmp.ds')
-        times['ds_save'].append(time.time() - t0)
+    filename = tempfile.gettempdir() + os.sep + 'tmp.ds'
 
-        t0 = time.time()
-        load_omas_ds(tempfile.gettempdir() + os.sep + 'tmp.ds')
-        times['ds_load'].append(time.time() - t0)
+    t0 = time.time()
+    odx = ods_2_odx(ods)
+    times['HM'].append(time.time() - t0)
 
-        t0 = time.time()
-        for k in range(stats_reps):
-            odx = load_omas_dx(tempfile.gettempdir() + os.sep + 'tmp.ds')
-        times['dx_load'].append((time.time() - t0) / float(stats_reps))
+    t0 = time.time()
+    save_omas_dx(odx, filename)
+    times['TW'].append(time.time() - t0)
 
-        t0 = time.time()
-        for k in range(stats_reps):
-            for kk in range(n):
-                odx['equilibrium.time_slice.%d.profiles_1d.psi' % kk]
-        times['odx_slice_access'].append((time.time() - t0) / n / float(stats_reps))
+    t0 = time.time()
+    odx = load_omas_dx(filename)
+    times['TR'].append(time.time() - t0)
 
-        t0 = time.time()
-        for k in range(stats_reps):
+    t0 = time.time()
+    ods = odx_2_ods(odx)
+    times['TM'].append(time.time() - t0)
+
+    t0 = time.time()
+    for k in range(stats_reps):
+        for kk in range(n):
+            odx['equilibrium.time_slice.%d.profiles_1d.psi' % kk]
+    times['TA'].append((time.time() - t0) / n / float(stats_reps))
+
+    t0 = time.time()
+    for k in range(stats_reps):
+        for kk in range(n):
+            odx['equilibrium.time_slice.:.profiles_1d.psi'][:, 0]
+    times['TS'].append((time.time() - t0) / n / float(stats_reps))
+
+    t0 = time.time()
+    for k in range(stats_reps):
+        for kk in range(n):
             odx['equilibrium.time_slice.:.profiles_1d.psi']
-        times['odx_bulk_access'].append((time.time() - t0) / float(stats_reps))
+    times['TB'].append((time.time() - t0) / n / float(stats_reps))
 
-    else:
-        times['ds_save'].append(numpy.nan)
-        times['ds_load'].append(numpy.nan)
-        times['dx_load'].append(numpy.nan)
-        times['odx_slice_access'].append(numpy.nan)
-        times['odx_bulk_access'].append(numpy.nan)
+print(samples)
+pprint(times)
 
-    if len(times['h5_save']) and times['h5_save'][-1] is numpy.nan and len(times['ds_save']) and times['ds_save'][-1] is numpy.nan:
-        samples = samples[:samples.index(n) + 1]
-        break
-
-    pprint(times)
-
-# plot
+# plot read/write scaling
 pyplot.figure()
-
-# save/load scaling
-for item in ['h5_save', 'h5_load', 'ds_save', 'ds_load', 'dx_load']:
-    pyplot.loglog(samples, times[item], label='OMAS ' + item.replace('_', ' '), lw=1.5, ls=['-', '--']['save' in item])
+for type in ['H', 'T']:  # hierarchical or tensor
+    for action in ['R', 'W']:  # Read, Write
+        pyplot.loglog(samples, times[type + action], label=type + action, lw=1.5, ls=['-', '--']['H' in type])
 pyplot.xlabel('# of Equilibrium Time Slices')
 pyplot.ylabel('Time [s]')
-pyplot.legend()
+pyplot.legend(loc='upper left', frameon=False)
+pyplot.title('Read/Write', y=0.85, x=0.7)
 
-# data access scaling
+# plot mapping scaling
 pyplot.figure()
-for item in ['ods_slice_access', 'odx_slice_access', 'ods_bulk_access', 'odx_bulk_access']:
-    pyplot.loglog(samples, numpy.array(times[item]) * 1E6, label='OMAS ' + item.replace('_', ' '), lw=1.5, ls=['-', '--']['slice' in item])
+for type in ['H', 'T']:  # hierarchical or tensor
+    for action in ['M']:  # Mapping
+        pyplot.loglog(samples, times[type + action], label=type + action, lw=1.5, ls=['-', '--']['H' in type])
 pyplot.xlabel('# of Equilibrium Time Slices')
-pyplot.ylabel('Time Per Read [$\mu$s]')
-pyplot.legend()
+pyplot.ylabel('Time [s]')
+pyplot.legend(loc='upper left', frameon=False)
+pyplot.title('Mapping', y=0.85, x=0.7)
+
+# plot access scaling
+pyplot.figure()
+for type in ['H', 'T']:  # hierarchical or tensor
+    for action in ['A', 'S', 'B']:  # Array access, Stripe access, Bulk access
+        pyplot.loglog(samples, times[type + action], label=type + action, lw=1.5, ls=['-', '--']['H' in type])
+pyplot.xlabel('# of Equilibrium Time Slices')
+pyplot.ylabel('Time [s]')
+pyplot.legend(loc='upper left', frameon=False)
+pyplot.title('Access', y=0.85, x=0.7)
 
 pyplot.show()
