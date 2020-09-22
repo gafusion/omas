60d24928ed7cdf87c836bc92061a4642cd57a5ed
David Eldon <eldond@fusion.gat.com>
2020-09-22 05:45:14 -0700

    Add start of list of formatted physics symbols for IMAS quantities
    - The list is not complete yet

---
+++ omas/omas_symbols.py
@@ -0,0 +1,12 @@
+"""
+Formatted symbols for physics quantities
+"""
+
+phys_sym = {}
+
+phys_sym['langmuir_probes.embedded.:.saturation_current_ion'] = '$I_{sat}$'
+phys_sym['langmuir_probes.embedded.:.n_e'] = '$n_e$'
+phys_sym['langmuir_probes.embedded.:.t_e'] = '$T_e$'
+phys_sym['langmuir_probes.embedded.:.t_i'] = '$T_i$'
+phys_sym['langmuir_probes.embedded.:.potential_plasma'] = r'$\phi_{plasma}'
+phys_sym['langmuir_probes.embedded.:.potential_floating'] = r'$\phi_f'
