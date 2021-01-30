e06a6065f196d87b843246fea0aa42a9bf0e9ab2
Orso Meneghini <orso82@gmail.com>
2021-01-29 23:48:24 -0800

    Add d3d magnetics ip

--- omas/omas_cocos.py
+++ omas/omas_cocos.py
@@ -679,7 +679,7 @@ _cocos_signals['magnetics.b_field_pol_probe.:.field.data']='POL'                
 _cocos_signals['magnetics.b_field_pol_probe.:.voltage.data']='POL'                                         # 1.500000 # b  b_field  [V]
 _cocos_signals['magnetics.b_field_tor_probe.:.field.data']='TOR'                                           # 1.500000 # b  b_field  [T]
 _cocos_signals['magnetics.b_field_tor_probe.:.voltage.data']='TOR'                                         # 1.500000 # b  b_field  [V]
-_cocos_signals['magnetics.ip.:.data']='?'                                                           #[ADD?]# 1.333333 # ip  [A]
+_cocos_signals['magnetics.ip.:.data']='TOR'                                                                # 1.333333 # ip  [A]
 
 # MHD
 _cocos_signals['mhd.ggd.:.b_field_tor.:.values']='?'                                             #[ADD?]# 2.800000 # b  b_field  _tor  [T]
