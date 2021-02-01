7af61dfd060a70d1e715c341a287120ace0692f3
Orso Meneghini <orso82@gmail.com>
2021-02-01 10:27:36 -0800

    Update machine mapping example

--- omas/examples/omas_dynamic_machine.py
+++ omas/examples/omas_dynamic_machine.py
@@ -9,34 +9,35 @@ Only the data that is queried in the ODS will be loaded.
 
 import os
 from omas import *
-from omfit.classes.omfit_eqdsk import OMFITgeqdsk
+from omfit_classes.omfit_eqdsk import OMFITgeqdsk
 from matplotlib import pyplot
 
 os.environ['OMAS_DEBUG_TOPIC'] = 'dynamic'
 
-# access some experimental data
-pyplot.figure()
+# load of some experiment quantities
 ods = ODS()
 with ods.open('machine', 'd3d', 168830, options={'EFIT_tree': 'EFIT01'}):
-    pyplot.plot(ods['equilibrium.time'], ods['equilibrium.time_slice.:.global_quantities.ip'])
-    pyplot.xlabel(f"[{ods.info('equilibrium.time')['units']}]")
-    pyplot.ylabel(f"[{ods.info('equilibrium.time_slice.:.global_quantities.ip')['units']}]")
+    ip = ods['equilibrium.time_slice.:.global_quantities.ip']
+    print(f"Max Ip is {max(ip)} [A]")
+
+# plot experiment equilibrium reconstruction w/ probes and PF coils
+pyplot.figure()
+ods = ODS()
+with ods.open('machine', 'd3d', 168830, options={'EFIT_tree': 'EFIT02'}):
+    ods.plot_equilibrium_CX(time=2.1)
+    ods.plot_overlay(wall=True, magnetics=True, pf_active=True)
+pyplot.show()
+
+# plot magnetics
+pyplot.figure()
+ods = ODS()
+with ods.open('machine', 'd3d', 168830):
+    lines = pyplot.plot(ods[f'magnetics.b_field_pol_probe.:.field.time'].T, ods[f'magnetics.b_field_pol_probe.:.field.data'].T)
+    pyplot.xlabel('Time [s]')
+    pyplot.ylabel('Field [T]')
 pyplot.show()
 
 # generate a gEQDSK file from experimental data
-ods1 = ODS()
-with ods1.open('machine', 'd3d', 168830, options={'EFIT_tree': 'EFIT01'}):
-    g0 = OMFITgeqdsk(None).from_omas(ods1, time=1.1)
-    # notice that subsequent MDS+ calls for the same data are cached
-    g1 = OMFITgeqdsk(None).from_omas(ods1, time=2.1)
-
-# generate another one gEQDSK file from experimental data
-ods2 = ODS()
-with ods2.open('machine', 'd3d', 168830, options={'EFIT_tree': 'EFIT02'}):
-    g2 = OMFITgeqdsk(None).from_omas(ods2, time=2.1)
-
-pyplot.figure()
-g0.plot()
-g1.plot()
-g2.plot()
-pyplot.show()
+ods = ODS()
+with ods.open('machine', 'd3d', 168830, options={'EFIT_tree': 'EFIT02'}):
+    gEQDSK = OMFITgeqdsk(None).from_omas(ods, time=1.1)
