bd91a6bbf85925ded9e5cccc6e84b4ac7aa1889a
Orso Meneghini <orso82@gmail.com>
2021-02-08 23:03:50 -0800

    Minor refactoring of nstx python mappings

--- omas/machine_mappings/nstx-u.py
+++ omas/machine_mappings/nstx-u.py
@@ -6,6 +6,7 @@ from omas.machine_mappings._common import *
 
 __all__ = []
 
+
 @machine_mapping_function(__all__)
 def MDS_gEQDSK_psi_nstx(ods, pulse=139047, EFIT_tree='EFIT01'):
     return MDS_gEQDSK_psi(ods, 'nstx-u', pulse, EFIT_tree)
@@ -13,16 +14,18 @@ def MDS_gEQDSK_psi_nstx(ods, pulse=139047, EFIT_tree='EFIT01'):
 
 @machine_mapping_function(__all__)
 def MDS_gEQDSK_bbbs_nstx(ods, pulse=139047, EFIT_tree='EFIT01'):
-    TDIs = {'r': f'\\{EFIT_tree}::TOP.RESULTS.GEQDSK.RBBBS', 'z': f'\\{EFIT_tree}::TOP.RESULTS.GEQDSK.ZBBBS', 'n': f'\\{EFIT_tree}::TOP.RESULTS.GEQDSK.NBBBS'}
+    TDIs = {
+        'r': f'\\{EFIT_tree}::TOP.RESULTS.GEQDSK.RBBBS',
+        'z': f'\\{EFIT_tree}::TOP.RESULTS.GEQDSK.ZBBBS',
+        'n': f'\\{EFIT_tree}::TOP.RESULTS.GEQDSK.NBBBS',
+    }
     res = mdsvalue('nstx-u', pulse=pulse, treename=EFIT_tree, TDI=TDIs).raw()
     res['n'] = res['n'].astype(int)
     for k in range(len(res['n'])):
-        ods[f'equilibrium.time_slice.{k}.boundary.outline.r'] = res['r'][k, :res['n'][k]]
-        ods[f'equilibrium.time_slice.{k}.boundary.outline.z'] = res['z'][k, :res['n'][k]]
+        ods[f'equilibrium.time_slice.{k}.boundary.outline.r'] = res['r'][k, : res['n'][k]]
+        ods[f'equilibrium.time_slice.{k}.boundary.outline.z'] = res['z'][k, : res['n'][k]]
+
 
 # =====================
 if __name__ == '__main__':
-    ods = ODS()
-    with ods.open('machine', 'nstx-u', 139047):
-        print(ods['equilibrium.time_slice.:.profiles_1d.psi'])
-    # run_machine_mapping_functions(['MDS_gEQDSK_psi_nstx'], globals(), locals())
+    run_machine_mapping_functions(__all__, globals(), locals())
