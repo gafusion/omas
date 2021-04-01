4fbda84d2519fb02a71e90e7fdab10bde5acc1ef
Orso Meneghini <orso82@gmail.com>
2021-03-31 22:39:28 -0700

    Fix issue with summary sample

--- omas/omas_sample.py
+++ omas/omas_sample.py
@@ -247,7 +247,7 @@ def core_transport(ods, time_index=0):
 
 
 @add_to_ODS
-def summary(ods, time_index=0):
+def summary(ods):
     """
     Add sample core_profiles data
     This method operates in in-place
@@ -262,7 +262,6 @@ def summary(ods, time_index=0):
 
     pr = load_omas_json(imas_json_dir + '/../samples/sample_summary_ods.json', consistency_check=False)['summary']
     ods['summary'].update(pr)
-    ods.set_time_array('summary.time', time_index, float(time_index))
     return ods
 
 
