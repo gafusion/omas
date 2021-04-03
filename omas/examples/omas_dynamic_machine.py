1586ff2f4a36dc312a61611b59e74264c575a7d8
Orso Meneghini <orso82@gmail.com>
2021-04-02 23:37:31 -0700

    Simplify omas_dynamic_machine_example

--- omas/examples/omas_dynamic_machine.py
+++ omas/examples/omas_dynamic_machine.py
@@ -18,14 +18,14 @@ os.environ['OMAS_DEBUG_TOPIC'] = 'dynamic'
 
 # load of some experiment quantities
 ods = ODS()
-with ods.open('machine', 'd3d', 168830, options={'EFIT_tree': 'EFIT01'}):
+with ods.open('d3d', 168830, options={'EFIT_tree': 'EFIT01'}):
     ip = ods['equilibrium.time_slice.:.global_quantities.ip']
     print(f"Max Ip is {max(ip)} [A]")
 
 # plot experiment equilibrium reconstruction w/ probes and PF coils
 pyplot.figure()
 ods = ODS()
-with ods.open('machine', 'd3d', 168830, options={'EFIT_tree': 'EFIT02'}):
+with ods.open('d3d', 168830, options={'EFIT_tree': 'EFIT02'}):
     ods.plot_equilibrium_CX(time=2.1)
     ods.plot_overlay(wall=True, magnetics=True, pf_active=True)
 pyplot.show()
@@ -33,7 +33,7 @@ pyplot.show()
 # plot magnetics
 pyplot.figure()
 ods = ODS()
-with ods.open('machine', 'd3d', 168830):
+with ods.open('d3d', 168830):
     lines = pyplot.plot(ods[f'magnetics.b_field_pol_probe.:.field.time'].T, ods[f'magnetics.b_field_pol_probe.:.field.data'].T)
     pyplot.xlabel('Time [s]')
     pyplot.ylabel('Field [T]')
@@ -42,7 +42,7 @@ pyplot.show()
 # generate a D3D gEQDSK file from experimental data
 pyplot.figure()
 ods = ODS()
-with ods.open('machine', 'd3d', 168830, options={'EFIT_tree': 'EFIT02'}):
+with ods.open('d3d', 168830, options={'EFIT_tree': 'EFIT02'}):
     gEQDSK = OMFITgeqdsk(None).from_omas(ods, time=1.1)
 gEQDSK.plot()
 pyplot.show()
@@ -50,7 +50,7 @@ pyplot.show()
 # generate a NSTX gEQDSK file from experimental data
 pyplot.figure()
 ods = ODS()
-with ods.open('machine', 'nstxu', 139047, options={'EFIT_tree': 'EFIT01'}):
+with ods.open('nstxu', 139047, options={'EFIT_tree': 'EFIT01'}):
     gEQDSK = OMFITgeqdsk(None).from_omas(ods, time=0.5)
 gEQDSK.plot()
 pyplot.show()
