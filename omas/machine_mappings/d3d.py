c33075df4fe60c9cbc8454d5f8e7e5a04b62fbad
Orso Meneghini <meneghini@fusion.gat.com>
2025-03-27 17:48:43 -0700

    Add function information to D3D coils

--- omas/machine_mappings/d3d.py
+++ omas/machine_mappings/d3d.py
@@ -395,7 +395,12 @@ def pf_active_hardware(ods, pulse):
         ods['pf_active.coil'][k]['identifier'] = fcid
         ods['pf_active.coil'][k]['element.0.name'] = fcid
         ods['pf_active.coil'][k]['element.0.identifier'] = fcid
-
+        if k < 6:
+            # `flux` function
+            ods['pf_active.coil'][k]["function.0.index"] = 0
+        else:
+            # `shaping` function
+            ods['pf_active.coil'][k]["function.0.index"] = 1
 
 @machine_mapping_function(__regression_arguments__, pulse=133221)
 def pf_active_coil_current_data(ods, pulse):
