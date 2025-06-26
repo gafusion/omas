335cba908a468745d8558b9687b6c0f00d131ced
AreWeDreaming <denks@fusion.gat.com>
2025-06-26 11:00:44 -0700

    Add convenience script for all data FUSE needs

---
+++ omas/examples/fuse_data_export.py
@@ -0,0 +1,68 @@
+import time
+import omas
+import sys
+from omas.omas_utils import printe
+from omas.machine_mappings import d3d
+from numpy import *
+
+
+def fuse_export(shot, EFIT_TREE, PROFILES_TREE, EFIT_RUN_ID=None, PROFILES_RUN_ID=None):
+    ods = omas.ODS()
+
+    tic = time.time()
+    printe("- Fetching ec_launcher data")
+    d3d.ec_launcher_active_hardware(ods, shot)
+
+    # printe("- Fetching nbi data")
+    # d3d.nbi_active_hardware(ods, shot)
+
+    printe("- Fetching core_profiles data")
+    d3d.core_profiles_profile_1d(ods, shot, PROFILES_tree=PROFILES_TREE, 
+                                 PROFILES_RUN_ID=PROFILES_RUN_ID)
+
+    printe("- Fetching wall data")
+    d3d.wall(ods, shot)
+
+    printe("- Fetching coils data")
+    d3d.pf_active_hardware(ods, shot)
+    d3d.pf_active_coil_current_data(ods, shot)
+
+    printe("- Fetching flux loops data")
+    d3d.magnetics_floops_data(ods, shot)
+
+    printe("- Fetching magnetic probes data")
+    d3d.magnetics_probes_data(ods, shot)
+
+    printe("- Fetching Thomson scattering data")
+    d3d.thomson_scattering_data(ods, shot)
+
+    printe("- Fetching charge exchange data")
+    d3d.charge_exchange_data(ods, shot, analysis_type="$(CER_analysis_type)")
+
+    printe("- Fetching summary data")
+    d3d.summary(ods, shot)
+
+    printe("- Fetching equilibrium data")
+    with ods.open('d3d', shot, options={'EFIT_tree': EFIT_TREE, "EFIT_RUN_ID": EFIT_RUN_ID}):
+        for k in range(len(ods["equilibrium.time"])):
+            ods["equilibrium.time_slice"][k]["time"]
+            ods["equilibrium.time_slice"][k]["global_quantities.ip"]
+            ods["equilibrium.time_slice"][k]["profiles_1d.psi"]
+            ods["equilibrium.time_slice"][k]["profiles_1d.f"]
+            ods["equilibrium.time_slice"][k]["profiles_1d.pressure"]
+            ods["equilibrium.time_slice"][k]["profiles_2d[0].psi"]
+            ods["equilibrium.time_slice"][k]["profiles_2d[0].grid.dim1"]
+            ods["equilibrium.time_slice"][k]["profiles_2d[0].grid.dim2"]
+            ods["equilibrium.time_slice"][k]["profiles_2d[0].grid_type.index"] = 1
+            ods["equilibrium.vacuum_toroidal_field.r0"]
+            ods["equilibrium.vacuum_toroidal_field.b0"]
+
+    printe(f"Data fetched via OMAS in {time.time()-tic:.2f} [s]")
+
+    printe("Saving ODS to $filename", end="")
+    tic = time.time()
+    ods.save("$filename")
+    printe(f" Done in {time.time()-tic:.2f} [s]")
+
+if __name__ == "__main__":
+    fuse_export(*sys.argv, *sys.kwargs)
