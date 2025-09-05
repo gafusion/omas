fe8a6b9b1f0b70e35322976e7aa8594b2fe4ab71
d3dsf <d3dsf-d3d-dl@fusion.gat.com>
2025-09-05 11:02:42 -0700

    Add interferometer

--- omas/examples/fuse_data_export.py
+++ omas/examples/fuse_data_export.py
@@ -19,11 +19,12 @@ def fuse_export(save_path, device, shot, EFIT_TREE, PROFILES_TREE,
     tic = time.time()
     if device.lower() != "d3d":
         raise ValueError(f"Unsupported device {device}. Only 'd3d' supported at present.")
+
     printe("- Fetching ec_launcher data")
     d3d.ec_launcher_active_hardware(ods, shot)
 
     # printe("- Fetching nbi data")
-    # d3d.nbi_active_hardware(ods, shot)
+    # d3d.nbi_active_hardware(ods, $shot)
 
     printe("- Fetching core_profiles data")
     d3d.core_profiles_profile_1d(ods, shot, PROFILES_tree=PROFILES_TREE, 
@@ -32,12 +33,12 @@ def fuse_export(save_path, device, shot, EFIT_TREE, PROFILES_TREE,
     printe("- Fetching wall data")
     d3d.wall(ods, shot)
 
-        printe("- Fetching magnetic hardware data")
-        d3d.magnetics_hardware(ods, $shot)
-
     printe("- Fetching coils data")
     d3d.pf_active_hardware(ods, shot)
     d3d.pf_active_coil_current_data(ods, shot)
+
+    printe("- Fetching magnetic hardware data")
+    d3d.magnetics_hardware(ods, shot)
 
     printe("- Fetching flux loops data")
     d3d.magnetics_floops_data(ods, shot)
@@ -48,8 +49,12 @@ def fuse_export(save_path, device, shot, EFIT_TREE, PROFILES_TREE,
     printe("- Fetching Thomson scattering data")
     d3d.thomson_scattering_data(ods, shot)
 
+    printe("- Fetching interferometer data")
+    d3d.interferometer_hardware(ods, shot)
+    d3d.interferometer_data(ods, shot)
+
     printe("- Fetching charge exchange data")
-    d3d.charge_exchange_data(ods, shot, analysis_type=CER_analysis_type)
+    d3d.charge_exchange_data(ods, shot, analysis_type="$(CER_analysis_type)")
 
     printe("- Fetching summary data")
     d3d.summary(ods, shot)
