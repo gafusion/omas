a046a244b7f874e1ff8df5980bbc8746719b1acf
AreWeDreaming <denks@fusion.gat.com>
2026-05-21 08:38:09 -0700

    Fix linestyle

--- omas/omas_plot.py
+++ omas/omas_plot.py
@@ -1099,8 +1099,8 @@ def equilibrium_summary(ods, time_index=None, time=None, fig=None, ggd_points_tr
                                             ods[f"equilibrium.time_slice.{time_index}.constraints.j_tor.:.position.psi"])
             plot_1d_equilbrium_quantity(ax, x, eq["profiles_1d.j_tor"] / 1.e6,
                                         xName, r"$\langle j_\mathrm{tor} / R \rangle$ [MA m$^{-2}$]", 
-                                        r"$j_\mathrm{tor}$", visible_x=omas_viewer, linestyle="None", marker=".",
-                                        color='red')
+                                        r"$j_\mathrm{tor}$", visible_x=omas_viewer, linestyle="solid",
+                                        color='blue')
             plot_1d_equilbrium_quantity(ax, x_constr, eq["constraints.j_tor.:.measured"] / 1.e6,
                                         xName, r"$\langle j_\mathrm{tor, meas} / R \rangle$ [MA m$^{-2}$]", 
                                         r"$j_\mathrm{tor}$", visible_x=omas_viewer, linestyle="None", marker=".",
